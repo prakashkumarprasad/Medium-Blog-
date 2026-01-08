619d8f60a9cf71e8a5a52239eccd42458c0e0349
Prakash Prasad <pg487898@gmail.com>
2026-01-08 21:24:04 +0530

    Initial commit

---
+++ src/pages/Blogs.tsx
@@ -0,0 +1,47 @@
+import { AppBar } from "../components/AppBar"
+import { BlogCard } from "../components/BlogCard"
+import { BlogSkeletonList } from "../components/BlogSkeleton"
+import { useBlogs } from "../hooks"
+
+export const Blogs = ()=>{
+    const {loading, blogs} = useBlogs()
+
+    if (loading) {
+        return (
+            <div>
+                <AppBar/>
+                <div className="flex justify-center">
+                    <div className="max-w-xl">
+                        <BlogSkeletonList />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <AppBar/>
+            <div className="flex justify-center">
+                <div className="max-w-xl">
+                    {blogs.length === 0 ? (
+                        <div className="text-center pt-10">
+                            <p className="text-gray-500">No blogs found. Be the first to create one!</p>
+                        </div>
+                    ) : (
+                        blogs.map(blog => (
+                            <BlogCard 
+                                key={blog.id}
+                                id={blog.id}
+                                authorName={blog.author.name || "Anonymous"}
+                                title={blog.title}
+                                content={blog.content}
+                                publishedDate={"3rd Nov 2025"}
+                            />
+                        ))
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
